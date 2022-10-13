40d77d3f175e7e15c20ae537eaacc9768e972d47
koneki88 <114171959+koneki88@users.noreply.github.com>
2022-10-13 18:44:27 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+//Даны целые числа K и N(N > 0).Вывести N раз число K
+using namespace std;
+//1
+//int main()
+//{
+	//int k, n;
+	//cin >> k;
+	//cin >> n;
+
+	//int i;
+	//for (i = 0; i < n; ++i)
+		//cout << k << endl;
+	
+//}
+
+
+
+
+//2
+
+//int main() {
+//	int a, b;
+//	cin >> a;
+//	cin >> b;
+//
+//	int i;
+//	for (i = a; a < b; ++a) {
+		cout << a << endl;
+//	}
+//}
+
+
+
+//3
+//#include <iostream>
+//using namespace std;
+
+//int main() {
+//	int a, b;
+//	cout << "A:";
+//	cin >> a;
+//	cout << "B:";
+//	cin >> b;
+//	for (int i = b - 1; i > a; --i)
+//		cout << i << endl;
+//	cout << b - a - 1;
+//	return 0;
+
